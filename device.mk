5b8d8189477ee1f255c705d1dcee92b4aa80a19d
danascape <danascape@gmail.com>
2021-09-01 07:55:43 +0200

    billie2: Initial device makefiles
    * Device is A/B.
    * Build updater_engine
    
    Change-Id: I0456aa0a9ab487ca7b85e922d8018055e7502554

---
+++ device.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oneplus/billie2/billie2-vendor.mk)
+
+# Screen density
+TARGET_SCREEN_HEIGHT := 720
+TARGET_SCREEN_WIDTH := 1600
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Update engine
+PRODUCT_PACKAGES += \
+    brillo_update_payload \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
