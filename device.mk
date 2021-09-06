5408bb3527970e95b9f2ce6d8392bb8728eb7fbd
danascape <danascape@gmail.com>
2021-09-06 07:26:08 +0200

    billie2: Set Soong namespace for device
    Change-Id: I6e19b1f27ecdb1faa1ed80558a109b8b16612912

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget
 
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += \
+    device/oneplus/billie2
+
 # Screen density
 TARGET_SCREEN_HEIGHT := 720
 TARGET_SCREEN_WIDTH := 1600
