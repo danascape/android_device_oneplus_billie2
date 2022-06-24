9e2e9c4cf7a91d92a217a72731e1c57935cc3acf
danascape <danascape@gmail.com>
2022-06-24 06:44:47 -0600

    billie2: Include qcom device sepolicy
    Change-Id: I8cac3aa3dc7c0d708f37a4e13ccf38ef2b5a97b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,15 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
 # Sepolicy - billie
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# Sepolicy - Common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
