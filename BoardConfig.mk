61606546a92908d1b0a914184ac2f7a1172c627b
danascape <danascape@gmail.com>
2022-07-16 17:14:50 -0600

    [TEMP] billie2: Build permissive
    Change-Id: If08b4421549f8740debe427bc8a96c883b1afd35

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  4096
